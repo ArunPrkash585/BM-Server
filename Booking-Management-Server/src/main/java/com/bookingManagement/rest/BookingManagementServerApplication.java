948d3ef964095bbe179f9fee577679e42283a1ed
arun <arun@Arun>
2017-12-29 00:11:26 +0530

    initial commit

---
+++ Booking-Management-Server/src/main/java/com/bookingManagement/rest/BookingManagementServerApplication.java
@@ -0,0 +1,12 @@
+package com.bookingManagement.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingManagementServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingManagementServerApplication.class, args);
+	}
+}
